43b15074c52a23e4249bee27d1abd114cc2d51ba
COD2hack <felipe_moraess@hotmail.com>
2024-02-02 17:47:33 -0300

    Interfaces com o Dialog

---
+++ Cap_11/Exemplos/zuserd.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#
+# zuserd
+# Lista, adiciona e remove usuários do sistema Z
+#
+# Requisitos: bantex.sh, dialog
+#
+# 01-02-2024 - MZKY
+
+# Localização do arquivo do banco de dados
+BANCO=usuarios.txt
+
+# Inclui o gerenciador do banco
+source bantex-4.sh || {
+	echo "Ops, ocorreu algum erro no gerenciador do banco."
+	return 1
+}
+
+while :
+do
+	acao=$( dialog --stdout \
+		--menu "Aplicativo Zuserd - Interface amigável" \
+		0 0 0 \
+		lista "Lista todos os usuários do sistema" \
+		adiciona "Adiciona um novo usuário no sistema" \
+		remove "Remove um usuário do sistema")
+	[ $? -ne 0 ] | echo -e '\033c' && exit 
+	
+	# Lida com os comandos recebidos
+	case "$acao" in
+
+		lista)
+			# Lista dos logins (apaga a primeira linha)
+			temp=$(mktemp -t temp_zuserd_XXX)
+			pega_campo 1 | sed 1d > "$temp"
+			dialog --title "Usuários" --textbox "$temp" 13 30
+			rm $temp
+			continue
+		;;
+
+		adiciona)
+			login=$(dialog --stdout --inputbox "Digite o login:" 0 0)
+			[ $? -ne 0 ] && continue
+			[ "$login" ] || continue
+
+			# Primeiro confere se já não existe esse usuário
+			tem_chave "$login" && {
+				msg="O usuário '$login' já foi cadastrado."
+				dialog --msgbox "$msg" 6 40
+				continue
+			}
+
+			# Ok, é um usuário novo, prossigamos
+			nome=$(dialog --stdout --inputbox "Nome Completo:" 0 0 )
+			[ $? -ne 0 ] && continue
+
+			idade=$(dialog --stdout --inputbox "Idade:" 0 0)
+			[ $? -ne 0 ] && continue
+
+			sexo=$(dialog --stdout --radiolist "Para escolher, utilize os botões UP ou DOWN (seta para cima ou para baixo) e pressione a tecla ESPAÇO para marcar a opção, por fim, confirme pressionando ENTER.\n\n\
+			Por favor, informe o sexo do usuário: " 0 0 0 \
+			F 'Feminino' on \
+			M 'Masculino' off \
+			NA 'Não informado' off)
+			[ $? -ne 0 ] && continue
+		
+			# Dados obtidos, hora de mascarar eventuais dois-pontos
+			nome=$(echo $nome | mascara)
+
+			# Tudo pronto, basta inserir
+			msg=$(insere_registro "$login:$nome:$idade:$sexo")
+			dialog --title "Resultado" --msgbox "$msg" 6 40
+		;;
+
+		remove)
+			# Obtém a lista de usuários
+			usuarios=$(pega_campo 1,2 | sed 1d)
+			usuarios=$(echo "$usuarios" | sed 's/:/ "/;s/$/"/')
+
+			login=$(eval dialog --stdout \
+				--menu \"Escolha o usuário a remover\" \
+				0 0 0 $usuarios)
+			[ $? -ne 0 ] && continue
+
+			msg=$(apaga_registro "$login")
+			dialog --title "Resultado" --msgbox "$msg" 6 40
+		;;
+	esac
+echo -e '\033c'
+done
+echo -e '\033c'
